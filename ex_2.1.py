7e538516a5646fc416269c99e3737ee0467abea8
yji21 <129173858+yji21@users.noreply.github.com>
2024-10-02 00:53:19 +0900

    Add files via upload

---
+++ ex_2.1.py
@@ -0,0 +1,39 @@
+import torch
+
+x = torch.arange(12, dtype=torch.float32)
+print(x)
+X = x.reshape(-1, 4)
+print(X)
+
+y = torch.zeros(2, 3, 4)
+print(y)
+print(torch.exp(X))
+
+
+before = id(y)
+y = y + torch.ones(2, 3, 4)
+print(before)
+print(y)
+print(id(y))
+
+Y = torch.zeros_like(x)
+print(Y)
+
+before = id(y)
+y = y + torch.ones(2, 3, 4)
+print( id(y) < before )
+print( id(y) > before)
+
+z = y 
+before = id(z)
+z += torch.ones(2, 3, 4)
+print( id(z) < before)
+print( id(z) > before)
+
+a = torch.arange(3).reshape((3, 1, 1))
+b = torch.arange(4).reshape((1, 4, 1))
+c = torch.arange(2).reshape((1, 1, 2))
+print("a : ", a)
+print("b : ", b)
+print("c : ", c)
+print(a+b+c)
